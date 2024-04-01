833f97b78fd4e0d506dc3d3df0a4b19dc6988591
Alejandro Donoso <32345035+ElStickman@users.noreply.github.com>
2024-04-01 20:09:22 -0300

    Update 1.TwoSum.php

--- Github/leetcode/1.TwoSum.php
+++ Github/leetcode/1.TwoSum.php
@@ -1,8 +1,10 @@
 /**
 *
-*
+* Both runs with the same code.
 * Runtime 11ms Beats 94.47%of users with PHP
 * Memory 19.76MB Beats 99.39% of users with PHP
+* 7ms Beats 99.45% of users with PHP 
+* 20.28 MB Beats 72.71% of users with PHP
 **/
 class Solution {
 
