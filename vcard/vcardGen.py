a606faf44dc344e8703e50254d0d4859dad7fee7
ElStickman <elstickman>
2024-04-05 22:15:51 -0300

    quality of life.

--- vcard/vcardGen.py
+++ vcard/vcardGen.py
@@ -11,9 +11,31 @@ import numpy as np
 from PIL import Image, ImageDraw
 import qrcode
 
+def generar_numero_aleatorio():
+    if np.random.rand() < .66:
+        return (round(np.random.rand(), 1) + 1) * (round(np.random.rand(), 1) + 1)
+    return 1
+def dibujar_espiral(radio_total, dotmultiplicator, color, radio_qr, messy):
+    numero_aleatorioA = numero_aleatorioB = isinvertido = 1
+    if np.random.rand() < np.random.rand():
+        numero_aleatorioA = generar_numero_aleatorio()
+    if np.random.rand() < np.random.rand():
+        numero_aleatorioB = generar_numero_aleatorio()
+    if np.random.rand() < 0.5:
+        isinvertido = generar_numero_aleatorio()
+    for i in range(radio_total*dotmultiplicator):
+        if messy:
+            angle = .1*i*isinvertido*(radio_qr/350/2)
+        else:
+            angle = .1*i*isinvertido*(radio_qr/350)
+        r = angle
+        
+        point_x = int(radio_total + r * np.cos(angle)*numero_aleatorioA)
+        point_y = int(radio_total + r * np.sin(angle)*numero_aleatorioB)
+        dibujo_fondo.rectangle([point_x, point_y, point_x + dot, point_y + dot], fill=color)
 # Información de la vCard
-nombre = "Alejandro"
-apellido = "Donoso"
+nombre = "QUIEN TE DIO "
+apellido = "PERMISO DE ESCANEARME"
 nombre_completo = f"{nombre} {apellido}"
 organizacion = "ElStickman codes"
 titulo = "Dueño"
@@ -44,21 +66,25 @@ NOTE:{nota}
 END:VCARD
 """.strip()
 
-print(vcard_info)
+#print(vcard_info)
 
 # Parámetros para el diseño
 radio_qr = 350
+dotmultiplicator = 10
 radio_exterior = radio_qr *2
-radio_total = radio_exterior + 2
+dot = radio_qr/30
+print(f"Tamaño esperado {radio_exterior*2}x{radio_exterior*2}")
+radio_total = radio_exterior
 grosor_borde = 2
 color_borde = 'black'
+
 
 # Generación del código QR
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=1,
+    box_size=radio_qr/32,
+    border=0,
 )
 qr.add_data(vcard_info)
 qr.make(fit=True)
@@ -68,13 +94,12 @@ qr_img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
 img_fondo = Image.new('RGB', (radio_total * 2, radio_total * 2), 'white')
 dibujo_fondo = ImageDraw.Draw(img_fondo)
 
-for i in range(20000):
-    angle = .2*i
-    r = angle
-    point_x = int(radio_total + r * np.cos(angle))
-    point_y = int(radio_total + r * np.sin(angle))
-    dot = 4*3 #* np.random.uniform(1, 3)
-    dibujo_fondo.rectangle([point_x, point_y, point_x + dot, point_y + dot], fill='black')
+
+dibujar_espiral(radio_total, dotmultiplicator, 'lightgrey', radio_qr, True)
+dibujar_espiral(radio_total, dotmultiplicator, 'silver', radio_qr, True)
+dibujar_espiral(radio_total, dotmultiplicator, 'grey', radio_qr, False)
+dibujar_espiral(radio_total, dotmultiplicator, 'black', radio_qr, False)
+
 
 # Añadir el borde
 dibujo_fondo.ellipse(
